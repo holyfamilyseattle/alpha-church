0044dd64cc5c6ab1f4034a42f5ff7413f70f8561
Ajith Jose <ajithcherukad@gmail.com>
2023-09-16 17:43:42 -0700

    JS change to open URLs in new tab

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,4 +81,12 @@
 
 		}
 
+	// Open URLs in new tab
+	var links = document.getElementsByTagName('a');
+      for (var i = 0; i < links.length; i++) {
+        if (/^(https?:)?\/\//.test(links[i].getAttribute('href'))) {
+          links[i].target = '_blank';
+        }
+      }
+
 })(jQuery);
